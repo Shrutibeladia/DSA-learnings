d40d2cb76e1ab2933164fa470630b579298d385e
Shruti  Beladia <74319118+Shrutibeladia@users.noreply.github.com>
2024-06-18 18:38:46 +0530

    add addnum

---
+++ AddNumbers.java
@@ -0,0 +1,9 @@
+public class AddNumbers {
+    public static void main(String args[]){
+        int num1 = 25;
+        int num2 = 45;
+        int sum = num1 + num2 ;
+        System.out.print(sum);
+
+    }
+}
